31f414be05179478330864e98f157bf2d1364d86
Alex Bunting <alex.bunting314@gmail.com>
2023-10-26 11:59:27 +1100

    Add display of application version

--- lib/widgets/settings_icon.dart
+++ lib/widgets/settings_icon.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 import '../injection.dart';
 import '../services/auth_service.dart';
@@ -14,12 +15,12 @@ class SettingsIcon extends StatelessWidget {
   Widget build(BuildContext context) {
     return PopupMenuButton<_SettingsMenu>(
       icon: const Icon(Icons.settings),
-      onSelected: _onSelected,
+      onSelected: (_SettingsMenu menuItem) => _onSelected(menuItem, context),
       itemBuilder: _buildMenu,
     );
   }
 
-  void _onSelected(_SettingsMenu menu) {
+  void _onSelected(_SettingsMenu menu, BuildContext context) {
     switch (menu) {
       case _SettingsMenu.profile:
         break;
@@ -31,6 +32,20 @@ class SettingsIcon extends StatelessWidget {
       case _SettingsMenu.copyAuthToken:
         _copyAuthTokenToClipboard();
         break;
+      case _SettingsMenu.about:
+        _showAboutDialog(context);
+        break;
+    }
+  }
+
+  void _showAboutDialog(BuildContext context) async {
+    final packageInfo = await PackageInfo.fromPlatform();
+    if (context.mounted) {
+      showAboutDialog(
+        context: context,
+        applicationName: 'Trapa',
+        applicationVersion: packageInfo.version,
+      );
     }
   }
 
@@ -51,6 +66,10 @@ class SettingsIcon extends StatelessWidget {
         value: _SettingsMenu.logout,
         child: Text('Logout'),
       ),
+      const PopupMenuItem(
+        value: _SettingsMenu.about,
+        child: Text('About'),
+      ),
       if (kDebugMode)
         const PopupMenuItem(
           value: _SettingsMenu.copyAuthToken,
@@ -65,4 +84,5 @@ enum _SettingsMenu {
   settings,
   logout,
   copyAuthToken,
+  about,
 }
