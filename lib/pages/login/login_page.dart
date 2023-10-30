1ce98858daacf1f11afc796ccfe71088341ff18c
Alex Bunting <alex.bunting314@gmail.com>
2023-10-30 16:13:13 +1100

    Fix email registration by handling user created auth result

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import '../../injection.dart';
 import '../../router.dart';
+import '../../services/crash_report_service.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -11,8 +12,24 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SignInScreen(
       actions: [
-        AuthStateChangeAction<SignedIn>((context, state) {
-          getIt<AppRouter>().goToHome(context);
+        AuthStateChangeAction((context, state) {
+          if (state is SigningIn) {
+            return;
+          }
+          final user = switch (state) {
+            SignedIn(user: final user) => user,
+            CredentialLinked(user: final user) => user,
+            UserCreated(credential: final cred) => cred.user,
+            _ => null,
+          };
+          if (user != null) {
+            getIt<AppRouter>().goToHome(context);
+          } else {
+            getIt<CrashReportService>().report(
+              'Unexpected state returned from Firebase Sign In: $state',
+              StackTrace.current,
+            );
+          }
         }),
       ],
     );
