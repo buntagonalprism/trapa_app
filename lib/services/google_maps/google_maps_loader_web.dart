eb5bb7eb9a5d6b876f250d35f29885665721472e
Alex Bunting <alex.bunting314@gmail.com>
2023-10-30 16:57:23 +1100

    Manage google maps JS load script entirely in dart

--- lib/services/google_maps/google_maps_loader_web.dart
+++ lib/services/google_maps/google_maps_loader_web.dart
@@ -8,15 +8,22 @@ import 'package:js/js.dart';
 bool _hasRequestedInit = false;
 final _initCompleter = Completer<void>();
 
+const _mapsApiKey = String.fromEnvironment('googleMapsApiKey');
+const _mapsLoaderScriptId = 'google-maps-js-loader';
+
 Future<void> initGoogleMaps() {
-  if (_hasRequestedInit) {
+  if (_hasRequestedInit || html.document.getElementById(_mapsLoaderScriptId) != null) {
     return Future.value();
   }
   _hasRequestedInit = true;
 
   onMapsInit = allowInterop(_onMapsInit);
-  html.document.dispatchEvent(html.CustomEvent("google-maps-api-key-loaded",
-      detail: const String.fromEnvironment('googleMapsApiKey')));
+
+  html.document.head!.append(html.ScriptElement()
+    ..src = 'https://maps.googleapis.com/maps/api/js?key=$_mapsApiKey&callback=onMapsInit'
+    ..id = _mapsLoaderScriptId
+    ..async = true
+    ..defer = true);
   return _initCompleter.future;
 }
 
